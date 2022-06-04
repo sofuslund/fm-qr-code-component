5c58ee5b5694ed5153b0eeccf743441f9535090e
Sofus Lund <sofuslund.dev@gmail.com>
2022-06-04 12:24:33 +0200

    Initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
 module.exports = {
-  content: ['./*.html'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    content: ['./*.html'],
+    theme: {
+        colors: {
+            'white': '#ffffff',
+            'light-grey': 'hsl(212, 45%, 89%)',
+            'grayish-blue': 'hsl(220, 15%, 55%)',
+            'dark-blue': 'hsl(218, 44%, 22%)',
+        },
+        fontFamily: {
+            'sans': ['Outfit']
+        },
+        extend: {},
+    },
+    plugins: [],
+};
